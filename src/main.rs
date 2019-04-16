f95ec290ffb759f25b0f77c7b7c1a65b4ab8f77b
Svetlana Marina <svetixbot@gmail.com>
2019-04-16 19:28:50 +1000

    Update readme and add go script

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
   let args: Vec<String> = env::args().collect();
   match &args.get(1).map(|v| &v[..]) {
-    Some("grid") => printer::print_8x8_grid(&board),
+    Some("--grid") => printer::print_8x8_grid(&board),
     _ => {
         printer::print_in_fen(&board)
     },
